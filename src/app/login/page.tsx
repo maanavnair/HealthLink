d5150070a132c6a18c22cde76be087ced7a48bb6
Maanav Nair <maanavnair20042@gmail.com>
2024-09-09 11:19:53 +0530

    made changes to logic

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,11 +2,33 @@
 import Link from 'next/link'
 import Image from 'next/image'
 import AuthImage from '@/images/AuthImage.png'
+import toast, { Toaster } from 'react-hot-toast'
+import { useState } from 'react'
+import { Checkbox } from '@/components/ui/checkbox'
 
 const Login = () => {
+
+    const [email, setEmail] = useState<string>("");
+    const [password, setPassword] = useState<string>("");
+
+    const [type, setType] = useState<string>("password");
+
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        if (email === "") {
+            toast.error("Email cannot be empty");
+        }
+        else if (password === "") {
+            toast.error("Password cannot be empty");
+        }
+    }
+
     return (
         <div className='w-screen min-h-screen bg-gray-200 flex justify-center items-center'>
-            <div className='bg-white w-[30vw] py-10 px-5 drop-shadow-xl'>
+            <form
+                className='bg-white w-[30vw] py-10 px-5 drop-shadow-xl rounded-xl'
+                onSubmit={handleSubmit}
+            >
                 <div className='flex justify-center'>
                     <Image src={AuthImage} alt='Auth' className='h-[20vh] w-[20vh]' />
                 </div>
@@ -15,15 +37,39 @@ const Login = () => {
                         <input
                             type='email'
                             placeholder='Email'
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
                             className='px-2 py-2 h-10 w-full border-2 border-gray-400 rounded-md'
                         />
                     </span>
-                    <span className='w-full'>
+                    <span className='w-full flex flex-col'>
                         <input
-                            type='password'
+                            type={type}
                             placeholder='Password'
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
                             className='px-2 py-2 h-10 w-full border-2 border-gray-400 rounded-md'
                         />
+                        <span className='mt-2 flex justify-center items-center'>
+                            <Checkbox
+                                id='show'
+                                onCheckedChange={
+                                    (e) => {
+                                        if (type === "text") {
+                                            setType("password")
+                                        }
+                                        else {
+                                            setType("text");
+                                        }
+                                    }
+                                }
+                            />
+                            <label
+                                htmlFor='show'
+                            >
+                                Show Password?
+                            </label>
+                        </span>
                     </span>
                 </div>
                 <div className='m-5 flex justify-center'>
@@ -31,7 +77,7 @@ const Login = () => {
                         href="/signup"
                         className='text-gray-600 underline'
                     >
-                        Don't have an account?Create one now!
+                        Don't have an account?
                     </Link>
                 </div>
                 <div>
@@ -41,7 +87,8 @@ const Login = () => {
                         LOGIN
                     </button>
                 </div>
-            </div>
+            </form>
+            <Toaster />
         </div>
     )
 }
--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -2,11 +2,40 @@
 import Image from 'next/image'
 import AuthImage from '@/images/AuthImage.png'
 import Link from 'next/link'
+import { useState } from 'react'
+import toast, { Toaster } from 'react-hot-toast'
+import { Checkbox } from '@/components/ui/checkbox'
 
 const Signup = () => {
+
+    const [username, setUsername] = useState<string>("");
+    const [email, setEmail] = useState<string>("");
+    const [password, setPassword] = useState<string>("");
+    const [type, setType] = useState<string>("password");
+
+
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        if (username === "") {
+            toast.error("Username cannot be empty")
+        }
+        else if (email === "") {
+            toast.error("Email cannot be empty");
+        }
+        else if (password === "") {
+            toast.error("Password cannot be empty");
+        }
+        else if (password.length < 6) {
+            toast.error("Password cannot be less than 6 characters");
+        }
+    }
+
     return (
         <div className='w-screen min-h-screen bg-gray-200 flex justify-center items-center'>
-            <div className='bg-white w-[30vw] py-10 px-5 drop-shadow-xl'>
+            <form
+                className='bg-white w-[30vw] py-10 px-5 drop-shadow-xl rounded-xl'
+                onSubmit={handleSubmit}
+            >
                 <div className='flex justify-center'>
                     <Image src={AuthImage} alt='Auth' className='h-[20vh] w-[20vh]' />
                 </div>
@@ -15,6 +44,8 @@ const Signup = () => {
                         <input
                             type='text'
                             placeholder='Username'
+                            value={username}
+                            onChange={(e) => setUsername(e.target.value)}
                             className='px-2 py-2 w-full border-2 border-gray-400 rounded-md'
                         />
                     </span>
@@ -22,15 +53,39 @@ const Signup = () => {
                         <input
                             type='email'
                             placeholder='Email'
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
                             className='px-2 py-2 h-10 w-full border-2 border-gray-400 rounded-md'
                         />
                     </span>
-                    <span className='w-full'>
+                    <span className='w-full flex flex-col'>
                         <input
-                            type='password'
+                            type={type}
                             placeholder='Password'
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
                             className='px-2 py-2 h-10 w-full border-2 border-gray-400 rounded-md'
                         />
+                        <span className='mt-2 flex justify-center items-center'>
+                            <Checkbox
+                                id='show'
+                                onCheckedChange={
+                                    (e) => {
+                                        if (type === "text") {
+                                            setType("password")
+                                        }
+                                        else {
+                                            setType("text");
+                                        }
+                                    }
+                                }
+                            />
+                            <label
+                                htmlFor='show'
+                            >
+                                Show Password?
+                            </label>
+                        </span>
                     </span>
                 </div>
                 <div className='m-5 flex justify-center'>
@@ -43,12 +98,14 @@ const Signup = () => {
                 </div>
                 <div>
                     <button
+                        type='submit'
                         className='w-full bg-gray-900 text-white py-3 rounded-md'
                     >
                         SIGNUP
                     </button>
                 </div>
-            </div>
+            </form>
+            <Toaster />
         </div>
     )
 }
